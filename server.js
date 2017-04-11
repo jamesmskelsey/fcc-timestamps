d14653808f697f7bcab17c2f049e5a7dddfc0990
James Kelsey <jamesmskelsey@gmail.com>
2017-04-11 04:03:55 +0000

    finish service

--- server.js
+++ server.js
@@ -1,8 +1,27 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(req,res) {
-    res.send("<html><body><h1>Hello, world!</h1></body></html>");
+app.get('/:timestamp', function(req,res) {
+    // Make a date.
+    var time = new Date(req.params.timestamp);
+    // Dirty error catching. Retry, just to be sure.
+    if (isNaN(time.valueOf())) {
+        time = new Date(Number(req.params.timestamp))
+    }
+    function formatDate(date) {
+        var monthNames = [
+            "January", "February", "March",
+            "April", "May", "June", "July", 
+            "August", "September", "October",
+            "November", "December"];
+        var day = date.getDate();
+        var month = monthNames[date.getMonth()];
+        var year = date.getFullYear();
+        return month + " " + day + ", " + year;
+    }
+    // So, is time a valid time? If not, reply with null, otherwise hey here's some dates for ya.
+    var responseDates = !isNaN(time.valueOf()) ? {unix: time.valueOf(), natural: formatDate(time)} : {unix: null, natural: null };
+    res.send(JSON.stringify(responseDates));
 });
 
 app.listen(8080, function(){
